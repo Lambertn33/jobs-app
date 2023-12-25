fa6d7bf1f578cb00149b66a44a4e1535cf2da017
Lambertn33 <lambertgithub@gmail.com>
2023-12-25 23:10:08 +0200

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Button } from 'flowbite-react';
+import { Home, Auth, Jobs, Job } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Button>Click me</Button>
-  )
-}
+    <Router>
+      <Switch>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/jobs/:id">
+          <Job />
+        </Route>
+        <Route path="/jobs">
+          <Jobs />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import { Home } from "./Home";
+import { Auth } from "./Auth";
+import { Job } from "./Job";
+import { Jobs } from "./Jobs";
+
+export { Home, Auth, Job, Jobs };
